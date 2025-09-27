58e5620d2f82ef66f01a64a968fa159b39b1a9eb
Shiva <shivachoudary53@gmail.com>
2025-09-27 23:03:41 +0530

    vercelChange

--- Backend/server.js
+++ Backend/server.js
@@ -14,6 +14,10 @@ connectDB();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
 
 app.use('/api/auth', authRoutes);
 app.use('/api/trips', tripRoutes);
